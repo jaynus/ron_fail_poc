58e1dc427fca3723880cd9379c7d04c567106bd7
Walter Pearce <jaynus@gmail.com>
2019-03-14 17:07:01 -0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use bitflags::bitflags;
+use option_set::option_set;
+
+option_set! {
+    pub struct Test: UpperCamel + u8 {
+        const One = 1;
+        const Two = 1 << 1;
+        const Three  = 1 << 2;
+    }
+}
+
+
+
+fn main() {
+    let flag = Test::One | Test::Two;
+
+    let json_ser = serde_json::ser::to_string(&flag).unwrap();
+    let ron_ser = ron::ser::to_string(&flag).unwrap();
+
+    let json_der: Test = serde_json::de::from_str(json_ser.as_str()).unwrap();
+    let ron_ser: Test = ron::de::from_str(json_ser.as_str()).unwrap();
+}
