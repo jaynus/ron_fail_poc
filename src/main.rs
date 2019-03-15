faa4ce57bee113176a44b397371976fdea4fcdf6
Walter Pearce <jaynus@gmail.com>
2019-03-15 11:04:50 -0700

    Bitflags_serial added

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use bitflags::bitflags;
+use bitflags::*;
 use option_set::option_set;
 
+#[macro_use]
+mod bitflags_serial;
 
 bitflags! {
     #[derive(serde::Serialize, serde::Deserialize)]
@@ -19,6 +21,14 @@ option_set! {
     }
 }
 
+bitflags_serial! {
+    pub struct TestBadTwo: u8 {
+        const One = 1;
+        const Two = 1 << 1;
+        const Three  = 1 << 2;
+    }
+}
+
 
 fn main() {
     let flag_good = TestGood::One | TestGood::Two;
@@ -29,12 +39,33 @@ fn main() {
     let json_der_good: TestGood = serde_json::de::from_str(json_ser_good.as_str()).unwrap();
     let ron_ser_good: TestGood = ron::de::from_str(ron_ser_good.as_str()).unwrap();
 
-
+    // option_set
     let flag_bad = TestBad::One | TestBad::Two;
 
     let json_ser_bad = serde_json::ser::to_string(&flag_bad).unwrap();
     let ron_ser_bad = ron::ser::to_string(&flag_bad).unwrap();
 
-    let json_der_bad: TestBad = serde_json::de::from_str(json_ser_bad.as_str()).unwrap();
-    let ron_ser_bad: TestBad = ron::de::from_str(ron_ser_bad.as_str()).unwrap();
+    let json_der_bad = serde_json::de::from_str::<TestBad>(json_ser_bad.as_str()).unwrap();
+    let ron_der_bad =  ron::de::from_str::<TestBad>(ron_ser_bad.as_str());
+    match ron_der_bad {
+        Ok(_) => { println!("* RON option_set succeeded!"); },
+        Err(_) => { println!("! RON option_set FAILED!"); },
+    }
+
+    // bitflags_serial
+    let flag_bad_two = TestBadTwo::One | TestBadTwo::Two;
+
+    let json_ser_bad_two = serde_json::ser::to_string(&flag_bad_two).unwrap();
+    let ron_ser_bad_two = ron::ser::to_string(&flag_bad_two).unwrap();
+
+    let json_der_bad_two = serde_json::de::from_str::<TestBadTwo>(json_ser_bad_two.as_str());
+    match json_der_bad_two {
+        Ok(_) => { println!("* JSON bitflags_serial succeeded!"); },
+        Err(_) => { println!("! JSON bitflags_serial FAILED!"); },
+    }
+    let ron_der_bad_two = ron::de::from_str::<TestBadTwo>(ron_ser_bad_two.as_str());
+    match ron_der_bad_two {
+        Ok(_) => { println!("* RON bitflags_serial succeeded!"); },
+        Err(_) => { println!("! RON bitflags_serial FAILED!"); },
+    }
 }
